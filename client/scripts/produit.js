ea5a0f10e186b653e00ff555b624ed2fcc603266
djeffkanda <djeffkanda@gmail.com>
2022-11-15 10:55:20 -0500

    added panier

--- client/scripts/produit.js
+++ client/scripts/produit.js
@@ -1,23 +1,22 @@
-
 ID_CLIENT = 1
 TOKEN_CLIENT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZENsaWVudCI6MSwicm9sZSI6ImNsaWVudCIsImlhdCI6MTYzNjc1MjI1MywiZXhwIjoxODM2NzUyMjUzfQ.qMcKC0NeuVseNSeGtyaxUvadutNAfzxlhL5LYPsRB8k"
 
 function add_item(id_item) {
     $.ajax({
-        url: "/clients/"+ID_CLIENT+"/panier",
-        method:"POST",
+        url: "/clients/" + ID_CLIENT + "/panier",
+        method: "POST",
         data: {"idProduit": id_item, "quantite": 1},
-        beforeSend: function (xhr){
-            xhr.setRequestHeader('Authorization', "Basic "+ TOKEN_CLIENT);
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', "Basic " + TOKEN_CLIENT);
         },
-        success: function( result ) {
+        success: function (result) {
             $('#item_counter').text(result.items.length)
         }
     });
 }
 
 
-function item_to_html(item){
+function item_to_html(item) {
     item_card = $('<div></div>')
         .addClass('card mb-4 rounded-3 shadow-sm');
 
@@ -27,33 +26,31 @@ function item_to_html(item){
 
     item_detail = $('<ul></ul>')
         .addClass('list-unstyled mt-3 mb-4')
-        .append('<li>Qte dispo :' + item.qte_inventaire +'</li>')
-        .append('<li>Categorie. :' + item.categorie.nom +'</li>');
+        .append('<li>Qte dispo :' + item.qte_inventaire + '</li>')
+        .append('<li>Categorie. :' + item.categorie.nom + '</li>');
 
     item_body = $('<div></div>')
         .addClass('card-body')
-        .append(' <h1 class="card-title text-center"> $' + item.prix +'</h1>')
+        .append(' <h1 class="card-title text-center"> $' + item.prix + '</h1>')
         .append(item_detail)
-        .append('<small class="small">'+item.description+'</small>' +
+        .append('<small class="small">' + item.description + '</small>' +
             '<p class="w-100 display-6 text-center">' +
-            '<button type="button" class="btn btn-primary position-relative" onclick="add_item('+item.id+')">' +
+            '<button type="button" class="btn btn-primary position-relative" onclick="add_item(' + item.id + ')">' +
             '<i class="bi bi-cart-plus"></i>' +
-            '</button>'+
+            '</button>' +
             '</p>');
 
 
     item_card.append(item_head).append(item_body);
 
-    return $('<div></div>').addClass('col-md-3') .append(item_card);
+    return $('<div></div>').addClass('col-md-3').append(item_card);
 }
 
 
-
-
-function chargerproduit(){
+function chargerproduit() {
     $.ajax({
         url: "/produits",
-        success: function( result ) {
+        success: function (result) {
             // console.log(result);
             $.each(result, function (key, value) {
                 item = item_to_html(value);
@@ -62,6 +59,32 @@ function chargerproduit(){
         }
     });
 }
+
+function chargerpanier() {
+    $.ajax({
+        url: "/clients/" + ID_CLIENT + "/panier",
+        method: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', "Basic " + TOKEN_CLIENT);
+        },
+        success: function (result) {
+            console.log(result)
+            $("#total").text(result.valeur);
+            $.each(result.items, function (key, value) {
+
+                item = $("<tr>" +
+                    "<td>" + value.nomProduit + "</td> " +
+                    "<td>" + value.prix + "</td> " +
+                    "<td>" + value.quantite + "</td> " +
+                    "<td>" + value.prix * value.quantite + "</td> " +
+                    "</tr>");
+
+                $('#cart_details').append(item);
+            });
+        }
+    });
+}
+
 
 $(function () {
 
--- routes/html.js
+++ routes/html.js
@@ -25,5 +25,9 @@ router.get('/produit', function(req, res){
     res.sendFile(path.join(__dirname + '/../client/produit.html'));
 });
 
+router.get('/panier', function(req, res){
+    res.sendFile(path.join(__dirname + '/../client/panier.html'));
+});
+
 
 module.exports = router
